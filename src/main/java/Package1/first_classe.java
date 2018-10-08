fc1c5074c50e23b4e05da53d87f9322c91735452
fhm <fhm@PORT_9XFTJC2.UMANIS.com>
2018-10-08 21:21:51 +0200

    Commite Branche2

--- src/main/java/Package1/first_classe.java
+++ src/main/java/Package1/first_classe.java
@@ -3,6 +3,6 @@ package Package1;
 public class first_classe {
 	 public static void main( String[] args )
 	    {
-	        System.out.println( "Hello firas!" );
+	        System.out.println( "Branche 2" );
 	    }
 }
